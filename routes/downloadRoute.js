41d8386ac11c5541b88a4b1afc6f7ab1cdbb9714
roeintheglasses <hrkjangir@gmail.com>
2020-07-26 04:05:59 +0530

    Fixes music download issues

--- routes/downloadRoute.js
+++ routes/downloadRoute.js
@@ -10,9 +10,9 @@ router.post('/music', (req, res) => {
     console.log("Got a request")
     console.log("Name : ", req.body.downloadMusicInput);
     if (!req.body.downloadMusicInput) {
-        return res.send({
-            error: 'You must provide a name'
-        })
+        res.render('music', {
+            error: 'Please enter a valid youtube URL'
+        });
     }
     downloadMusic(req, res)
 })
@@ -33,22 +33,27 @@ router.post('/video', (req, res) => {
 module.exports = router;
 
 //Music Download function
-function downloadMusic(req, res) {
-    const name = req.body.downloadMusicInput + " audio";
-    var saveLocation = name + '-byApollo.mp3';
-    console.log("Name in download function : ", name);
-    youtube.searchOne(name).then(result => {
-        const link = result.link
-        console.log("Got link: " + link);
-        var stream = ytdl(link);
+async function downloadMusic(req, res) {
+    const url = req.body.downloadMusicInput;
+    if (ytdl.validateURL(url)) {
+        const videoInfo = await ytdl.getInfo(url);
+        const video = {
+            title: videoInfo.title,
+            url: videoInfo.video_url
+        };
+        var stream = ytdl(video.url);
+        var str = video.title;
+        var name = str.replace(/[^a-zA-Z ]/g, "");
+        var saveLocation = name + ' audio.mp3';
+
         var proc = new ffmpeg({
-                source: stream
-            })
+            source: stream
+        })
             .withAudioCodec('libmp3lame')
             .toFormat('mp3')
-            .on('end', function() {
+            .on('end', function () {
                 console.log('file has been converted succesfully');
-                res.download(saveLocation, function(err) {
+                res.download(saveLocation, function (err) {
                     if (err) {
                         console.log(err)
                         return
@@ -63,13 +68,56 @@ function downloadMusic(req, res) {
                     });
                 });
             })
-            .on('error', function(err, stdout, stderr) {
+            .on('error', function (err, stdout, stderr) {
                 console.log('Error in proc: ' + err.message);
             })
             .saveToFile(saveLocation);
-    }).catch(error => {
-        console.log('Error in search : ' + error.message);
-    })
+    }
+    else {
+        res.render('music', {
+            error: 'Please enter a valid youtube URL'
+
+        });
+    }
+
+
+    // old search method Might work on it later
+    // const name = req.body.downloadMusicInput + " audio";
+    // var saveLocation = name + '-byApollo.mp3';
+    // console.log("Name in download function : ", name);
+    // youtube.searchOne(name).then(result => {
+    //     const link = result.link
+    //     console.log("Got link: " + link);
+    //     var stream = ytdl(link);
+    //     var proc = new ffmpeg({
+    //             source: stream
+    //         })
+    //         .withAudioCodec('libmp3lame')
+    //         .toFormat('mp3')
+    //         .on('end', function() {
+    //             console.log('file has been converted succesfully');
+    //             res.download(saveLocation, function(err) {
+    //                 if (err) {
+    //                     console.log(err)
+    //                     return
+    //                 }
+    //                 //Deleting File after sending it to the user
+    //                 fs.unlink(saveLocation, (err) => {
+    //                     if (err) {
+    //                         console.error(err)
+    //                         return
+    //                     }
+    //                     console.log("File Deleted!")
+    //                 });
+    //             });
+    //         })
+    //         .on('error', function(err, stdout, stderr) {
+    //             console.log('Error in proc: ' + err.message);
+    //         })
+    //         .saveToFile(saveLocation);
+    // }).catch(error => {
+    //     console.log('Error in search : ' + error.message);
+    // })
 }
 
 async function downloadVideo(req, res) {
